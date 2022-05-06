7cbbd7792225f9b291bfe82b523259faa01a6a76
batl64 <91633690+batl64@users.noreply.github.com>
2022-05-06 19:43:31 +0300

    Add files via upload

--- src/ex/Main.java
+++ src/ex/Main.java
@@ -1,16 +1,26 @@
 package ex;
 import java.util.Arrays;
-
+/**
+ * Універсальний клас
+ * @author 1
+ *
+ */
 public class Main {
-    
+	/**
+	 * Метод в якому обчислюється матриця.
+	 * @param N кількість рядків і стовпців матриці.
+	 * @param x початкове число відрахунку.
+	 */
     public static void calculation(int N, int x) {
     	 int [][] matrix = new int[N][N];
-    	 int n=0;
+    	 short n=0;
+    	 /*Заповнення матриці*/
          for(int[] row:matrix) {
         	 for(int i:row) 
         		 Arrays.fill(row,n,++n,x++);   
         	 n=0;    
         	 }
+
          for(int[] row:matrix)
              System.out.println(Arrays.toString(row));
     }
